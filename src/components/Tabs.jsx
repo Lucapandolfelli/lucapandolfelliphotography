ca2c760834f2bb7b90a39203631b76f0585cb27c
lucapandolfelli <lucapandolfelli2002@gmai.com>
2023-03-18 22:56:39 -0300

    new changes

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,7 +1,7 @@
 export default function Tabs ({ categories, setCurrentTab, currentTab }) {
   return (
     <section className='h-[40px] md:h-[50px] mb-[1rem] md:mb-[1.5rem] text-[#444]'>
-      <ul className='overflow-x-auto flex h-full scroll-smooth'>
+      <ul className='tabs-wrapper overflow-x-auto flex h-full scroll-smooth'>
         {categories?.map((tab, index) => (
           <li key={tab.id} className='min-w-[150px] md:w-[calc(100%/6)]'>
             <button 
