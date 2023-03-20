941322dd367c2ea4305ec4950b3121cd5dee0c56
lucapandolfelli <lucapandolfelli2002@gmai.com>
2023-03-20 09:07:39 -0300

    new changes

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -6,7 +6,7 @@ export default function Tabs ({ categories, setCurrentTab, currentTab }) {
           <li key={tab.id} className='min-w-[150px] md:w-[calc(100%/6)]'>
             <button 
               onClick={() => setCurrentTab(index)}
-              className={ currentTab === index ? 'uppercase bg-gray-300 dark:bg-[#252525] text-sm w-full h-full flex justify-center items-center' : 'uppercase bg-gray-100 dark:bg-[#333] text-sm w-full h-full flex justify-center items-center hover:bg-gray-200 dark:hover:bg-[#616161]'}>
+              className={ currentTab === index ? 'uppercase bg-gray-300 dark:bg-[#252525] text-sm font-medium dark:text-[#fff] w-full h-full flex justify-center items-center' : 'uppercase bg-gray-100 dark:bg-[#333] text-sm w-full h-full flex justify-center items-center hover:bg-gray-200 dark:hover:bg-[#616161]'}>
                 { navigator.language || navigator.userLanguage ? tab.title_en : tab.title }
               </button>
           </li>
